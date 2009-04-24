d213628835a32fec4c8a3f6d8930580081c217bb
Pete Skomoroch <pete@datawrangling.com>
2009-04-24 18:48:23 -0400

    changed default root

--- clusterservice/config/routes.rb
+++ clusterservice/config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => "jobs" 
   map.resources :jobs
 
   # The priority is based upon order of creation: first created -> highest priority.
