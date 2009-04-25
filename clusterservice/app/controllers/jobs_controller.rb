d6ad113a0055328d94e48e085f57525c89c19c5f
Pete Skomoroch <pete@datawrangling.com>
2009-04-24 21:46:00 -0400

    add pagination for job index page

--- clusterservice/app/controllers/jobs_controller.rb
+++ clusterservice/app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.xml
   def index
-    @jobs = Job.all
+    @jobs = Job.paginate :page => params[:page], :order => 'created_at DESC', :per_page =>10
 
     respond_to do |format|
       format.html # index.html.erb
