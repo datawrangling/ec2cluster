7f5376bfb27b31b001ccf3e5d22c5d530e073b7e
Pete Skomoroch <pete@datawrangling.com>
2009-05-28 17:23:51 -0400

    fleshing out command_runner.rb

--- lib/bootscripts/ubuntu_installs.sh
+++ lib/bootscripts/ubuntu_installs.sh
@@ -189,7 +189,7 @@ then
   # quick smoke test of multinode openmpi run, 
   su - elasticwulf -c "mpirun -np $CPU_COUNT --hostfile /home/elasticwulf/openmpi_hostfile /home/elasticwulf/hello > cluster_mpi_smoketest.txt"
 
-  # kick off ruby command_runner.rb script
+  # kick off ruby command_runner.rb script (only on master node)
   su - elasticwulf -c "ruby /home/elasticwulf/elasticwulf-service/lib/command_runner.rb"  
   
   
